9312e02ccde6f21135c3912bc18a7edf2cdcb408
Damnjan Lukovic <damnjan.lukovic@symphony.is>
2018-10-12 13:21:57 +0200

    Refactor

--- src/generateJoke.js
+++ src/generateJoke.js
@@ -1,47 +1,47 @@
 import Markov from 'markovchain';
 
+const wordCount = sentence => sentence.split(' ').length;
+const isQuestion = sentence => sentence.match(/^(why|what|who|where|did|do)/i);
+const endsWithPunctuation = sentence => sentence.match(/[.,!?]$/);
+
+const addPunctuation = sentence => {
+	if (isQuestion(sentence)) {
+		sentence += '?';
+	} else if (!endsWithPunctuation(sentence)) {
+		sentence += '.';
+	}
+	return sentence;
+};
+
+const generateSentence = (chain) => {
+	const sentence = chain.start(useUpperCase).end(endFn).process();
+	return addPunctuation(sentence);
+};
+
+const useUpperCase = (wordList) => {
+		const tmpList = Object.keys(wordList).filter((word) => {
+			return word[0] >= 'A' && word[0] <= 'Z'
+		});
+		return tmpList[~~(Math.random() * tmpList.length)]
+	}
+;
+
+const endFn = sentence => {
+	const length = wordCount(sentence);
+	return length >= 15 && !sentence.match(/(a|an|the|if|or|by|but|I|,)$/i);
+};
+
 export const generateJoke = (seed) => {
 	const chain = new Markov(seed);
+	let firstSentence, secondSentence = '';
 
-	// Use first word for a generated sentence only if it is capitalized
-	const useUpperCase = (wordList) => {
-			const tmpList = Object.keys(wordList).filter((word) => {
-				return word[0] >= 'A' && word[0] <= 'Z'
-			});
-			return tmpList[~~(Math.random() * tmpList.length)]
-		}
-	;
-
-	// Ends the sentence if it's longer than 15 words AND doesn't end on specific words
-	const endFn = sentence => {
-		const length = sentence.split(' ').length;
-		return length >= 15 && !sentence.match(/(a|an|the|if|or|by|but|I|,)$/i);
-	};
-
-	// First sentence
-	let first;
-	// Keep generating until sentence is at least 3 words long
 	do {
-		first = chain.start(useUpperCase).end(endFn).process();
-	} while (first.split(' ').length <= 3);
+		firstSentence = generateSentence(chain);
+	} while (wordCount(firstSentence) <= 3);
 
-	// Is it a question?
-	if (first.match(/^(why|what|who|where|did|do)/i)) {
-		first += '?';
-	} else if (!first.match(/[.,!?]$/)) {
-		first += '.';
+	if (wordCount(firstSentence) < 15) {
+		secondSentence = generateSentence(chain);
 	}
 
-	// Generate another sentence
-	let second = chain.start(useUpperCase).end(endFn).process();
-	if (!second.match(/[.,!?]$/)) {
-		second += '.';
-	}
-
-	// If first sentence is longer than 15, don't write second one
-	if (first.split(' ').length >= 15) {
-		second = '';
-	}
-
-	return `${first} ${second}`;
+	return `${firstSentence} ${secondSentence}`;
 };
